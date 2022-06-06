85fecb42a8ebf06e0ee311414fca61222aa64513
auckfrcade <69751942+auckfrcade@users.noreply.github.com>
2022-06-06 11:10:10 +0100

    Update app.js

--- app.js
+++ app.js
@@ -24,10 +24,10 @@ app.use(cookieParser());
 
 
 
-app.use('/welsh_data/', indexRouter);
-app.use('/welsh_data/users', usersRouter);
-app.use('/welsh_data/sample_data', sampledataRouter);
-app.use('/welsh_data/detailpage', detailpageRouter);
+app.use('/Welsh_data/', indexRouter);
+app.use('/Welsh_data/users', usersRouter);
+app.use('/Welsh_data/sample_data', sampledataRouter);
+app.use('/Welsh_data/detailpage', detailpageRouter);
 app.use(express.static('public'));
 
 
